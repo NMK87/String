11f14c7925788e7e0cd7740adadd31e406573bc2
NMK87 <nehaprabhakar0801@gmail.com>
2024-02-06 20:24:26 +0530

    String_04

---
+++ String/RotateString.java
@@ -0,0 +1,12 @@
+package org.interviewPreparation.Strings;
+
+public class RotateString {
+    public static void main (String args[]){
+        System.out.println(rotateString("abcde","cdeab"));
+    }
+
+    public static boolean rotateString(String s, String goal) {
+        return (s.length() == goal.length() && (s+s).contains(goal));
+
+    }
+}
