ecf561ba1b234c40be1a187166f383be3008936f
NMK87 <nehaprabhakar0801@gmail.com>
2024-02-19 19:48:25 +0530

    Strings_04

---
+++ String/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+package org.interviewPreparation.Strings;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+    public static void main(String args[]){
+        String strs[]={"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String strs[]){
+        Arrays.sort(strs);
+        String str1=strs[0];
+        String str2=strs[strs.length-1];
+        int i=0;
+        while(i<str1.length()){
+
+            if(str1.charAt(i)==str2.charAt(i)){
+                 i++;
+            }
+            else{
+                break;
+            }
+        }
+        return i==0?"": str1.substring(0,i);
+    }
+}
